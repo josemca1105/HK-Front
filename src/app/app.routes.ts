900532fecab4ef45ff8bb5ad13c2987c1d8ede46
josemca105 <jm.calderon1105@gmail.com>
2024-08-20 17:08:19 -0400

    Agregada pagina home y componentes login, register y 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { Error404Component } from './componentes/error404/error404.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: '',
+    component: NavbarComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'inicio',
+        pathMatch: 'full'
+      },
+      {
+        path: 'inicio',
+        component: HomeComponent
+      },
+      {
+        path: '404',
+        component: Error404Component
+      }
+    ]
+  },
+  { path: '**', component: Error404Component }
+];
