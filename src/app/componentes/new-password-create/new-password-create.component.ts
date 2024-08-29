7eb8402c9ba7819723c668d829e2eb390e12f059
josemca105 <jm.calderon1105@gmail.com>
2024-08-29 12:07:45 -0400

    Creados componentes para recuperar contraseña

---
+++ src/app/componentes/new-password-create/new-password-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-password-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-password-create.component.html',
+  styleUrl: './new-password-create.component.scss'
+})
+export class NewPasswordCreateComponent {
+
+}
