ad6e9e4a0370edf5bf534c1dacd29ad9f1398678
josemca105 <jm.calderon1105@gmail.com>
2024-09-05 10:57:40 -0400

    Creada tabla para mostrar todas las captaciones (admin)

--- src/app/admin/captaciones-admin-table/captaciones-admin-table.component.ts
+++ src/app/admin/captaciones-admin-table/captaciones-admin-table.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CaptacionesService } from '../../services/captaciones.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-captaciones-admin-table',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './captaciones-admin-table.component.html',
   styleUrl: './captaciones-admin-table.component.scss'
 })
 export class CaptacionesAdminTableComponent {
+  captaciones: any[] = [];
 
+  constructor(private captacionesService: CaptacionesService, private router: Router) {}
+
+  ngOnInit() {
+    this.loadCaptaciones();
+  }
+
+  loadCaptaciones() {
+    this.captacionesService.getCaptaciones()
+      .subscribe({
+        next: (response) => {
+          console.log('Captaciones fetched successfully', response);
+          this.captaciones = response;
+        },
+        error: (error) => {
+          console.error('Error getting captaciones', error);
+        }
+      });
+  }
+
+  editCaptacion(id: number) {
+    this.router.navigate(['/captaciones-admin-edit', id]);
+  }
+
+  // deleteuser(id: number) {
+  //   this.captacionesService.deleteCaptacion(id)
+  //     .subscribe({
+  //       next: (response) => {
+  //         console.log('User deleted successfully', response);
+  //         this.loadUsers();
+  //       },
+  //       error: (error) => {
+  //         console.error('Error deleting user', error);
+  //       }
+  //     });
+  // }
+
+  capitalizeFirstLetter(f_name: string): string {
+    if (!f_name) return '';
+    return f_name.charAt(0).toUpperCase() + f_name.slice(1).toLowerCase();
+  }
 }
