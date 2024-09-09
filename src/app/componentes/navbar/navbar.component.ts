61752261f066514df6fdd8d661b810647aec5c71
Katherine Leal <katleald0412@gmail.com>
2024-09-09 14:33:22 -0400

    Diseño de Navbar, Diseño de tabla de Usuarios sin responsive, inicio del diseño del modal de Crear Usuario

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -8,15 +8,14 @@ import { NgIf } from '@angular/common';
   standalone: true,
   imports: [RouterLink, RouterOutlet, NgIf],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
   user: any = null;
+  dropdownOpen: boolean = false; // Controla la visibilidad del dropdown
+  menuOpen: boolean = false; // Controla la visibilidad del menú de hamburguesa
 
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
     this.authService.getUser().subscribe({
@@ -26,8 +25,16 @@ export class NavbarComponent {
       },
       error: (error) => {
         console.error('Failed to get user', error);
-      }
+      },
     });
+  }
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen; // Alterna el estado del dropdown
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen; // Alterna el estado del menú de hamburguesa
   }
 
   logout() {
@@ -35,11 +42,13 @@ export class NavbarComponent {
       next: (response) => {
         console.log('Logout successful', response);
         this.user = null;
+        this.dropdownOpen = false;
+        this.menuOpen = false; // Cierra el menú al cerrar sesión
         this.router.navigate(['/login']);
       },
       error: (error) => {
         console.error('Logout failed', error);
-      }
+      },
     });
   }
 }
